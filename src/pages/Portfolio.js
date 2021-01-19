a3d3c22e87e088e1895f80d0692a304517971aac
layc41 <64671431+layc41@users.noreply.github.com>
2021-01-19 10:57:43 -0600

    added technologies

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,42 +7,48 @@ function Portfolio() {
             image: '../assets/img/movie-lot.png',
             deployed: 'https://movielot.herokuapp.com/',
             github: 'https://github.com/layc41/final-countdown',
-            description: 'As movie lovers, we wanted an app that would introduce us to new and old movies to change our normal variety. Then save the movies to remind us to watch them.'
+            description: 'As movie lovers, we wanted an app that would introduce us to new and old movies to change our normal variety. Then save the movies to remind us to watch them.', 
+            technologies: 'GraphQL, Mongo, React, JavaScript, Apollo-Server, Apollo-Client, LocalStorage, React-Bootstrap, Node.js, Heroku, React Hooks, JWT-decode, Third-Party API, Dotenv, PWA, Express, Bcrypt'
         },
         {
             name: 'Instartist',
             image: '../assets/img/instartist.png',
             deployed: 'https://instartist.herokuapp.com/',
             github: 'https://github.com/layc41/instartist',
-            description: 'Instartist is a social media application for virtually exhibiting works created by artists; provides a community to display and share the perspective of works of art of all kinds, where users can interact with these works and leave their comments. Both artists and users will create their accounts where they can upload a profile picture and information about themselves.'
+            description: 'Instartist is a social media application for virtually exhibiting works created by artists; provides a community to display and share the perspective of works of art of all kinds, where users can interact with these works and leave their comments. Both artists and users will create their accounts where they can upload a profile picture and information about themselves.',
+            technologies: 'Node.js, Bcrypt, dotenv, MySQL, Express, Express-Session, Handlebars, Sequelize, Cloudinary, Multer, JS, OOP, MVC, API, Bootstrap, Heroku' 
         },
         {
             name: 'Password Generator',
             image: '../assets/img/password-generator.png',
             deployed: 'https://layc41.github.io/password-generator/',
             github: 'https://github.com/layc41/password-generator',
-            description: 'This is a customizable random user password generator. It asks for length of password from 8 - 128 characters. Then asks for character types to be included in the password. Types of characters available are lowercase, uppercase, numbers, and symbols. If the amount of characters are out of range and no character types are selected, then a password cannot be generated.'
+            description: 'This is a customizable random user password generator. It asks for length of password from 8 - 128 characters. Then asks for character types to be included in the password. Types of characters available are lowercase, uppercase, numbers, and symbols. If the amount of characters are out of range and no character types are selected, then a password cannot be generated.', 
+            technologies: 'JavaScript'
         },
         {
             name: 'Tech Blog',
             image: '../assets/img/tech-blog.png',
             deployed: 'https://frozen-sierra-64365.herokuapp.com/',
             github: 'https://github.com/layc41/tech-blog',
-            description: 'A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.'
+            description: 'A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.',
+            technologies: 'JavaScript, Node.js, MongoDB, Handlebars, MVC, dotenv, MySQL'
         },
         {
             name: 'Refactored Ecommerce Application with Redux',
             image: '../assets/img/shop-redux.png',
             deployed: 'https://ancient-castle-97391.herokuapp.com/',
             github: 'https://github.com/layc41/shop-shop-redux',
-            description: 'Redux is the industry standard for managing complex state in a large-scale React application. This is a refactored e-commerce platform that was created by replacing the way it handles complex state management using the Context API with Redux, the open-source JavaScript library that it’s emulating.'
+            description: 'Redux is the industry standard for managing complex state in a large-scale React application. This is a refactored e-commerce platform that was created by replacing the way it handles complex state management using the Context API with Redux, the open-source JavaScript library that it’s emulating.',
+            technologies: 'JavaScript, Node.js, MongoDB, Redux, MVC, Stripe, GraphQL'
         },
         {
             name: 'Bored at Home',
             image: '../assets/img/bored.png',
             deployed: 'https://josephptflanagan.github.io/project-1',
             github: 'https://github.com/layc41/project-1',
-            description: 'Users want to learn new things and be entertained. That’s where Bored at Home comes into play. Our site is recommended for those who are bored at home, waiting at the doctor’s office, or in a random department store where a significant other might be trying on clothes. It’s sure to be a good time for all parties involved.'
+            description: 'Users want to learn new things and be entertained. That’s where Bored at Home comes into play. Our site is recommended for those who are bored at home, waiting at the doctor’s office, or in a random department store where a significant other might be trying on clothes. It’s sure to be a good time for all parties involved.',
+            techologies: 'Third Party APIs, Pure CSS, HTML/CSS, JS, AJAX, JQuery'
         }
     ]
 
@@ -67,6 +73,7 @@ function Portfolio() {
                                             <div className="entry-content"><a href={project.deployed} target="_blank">Visit the Site</a></div>
                                             <div className="entry-content">
                                                 <p>{project.description}</p>
+                                                <p>Technologies: {project.technologies}</p>
                                             </div>
                                         </article>
                                     </div>
